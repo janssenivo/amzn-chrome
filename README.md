edd48bc35dbc497905b1297b79269c10a4c02585
Ivo Janssen <jansseni@amazon.com>
2022-05-18 16:59:03 -0500

    configurable API key and other settings

--- README.md
+++ README.md
@@ -3,9 +3,25 @@
 This Chrome extension simply adds an AMZN stock quote to your extension bar. Nothing more nothing less.
 
 
+## Configuration
 
-### TODO 
+This extension needs an API Key to the Yahoo Finance API via rapidapi.com. Here's how you can sign up for a free API key:
 
-- Configurable Yahoo Finance API Key
-- Configurable update period
-- Configurable stock symbol???
+1. Go to https://rapidapi.com/
+2. Click ["Sign-Up"](https://rapidapi.com/auth/sign-up]) in the top right corner and create a free account
+3. Locate the [Finance / YH Finance API](https://rapidapi.com/apidojo/api/yh-finance/)
+4. Navigate to the "market/v2/get-quotes" API
+5. Click "Subscribe to test" and pick the $0.00 free plan which gives you 500 requests per month (or about 16 requests per day)
+6. From the right most test pane, copy the value for ```X-RapidAPI-Key```. It is a 50-character string.
+
+
+Now configure the extension to use your new API key.
+
+1. "Pin" the extension from the extension menu so it's always visible
+1. Right click on the extension's icon and click "Options". 
+2. Paste the API key in the "RAPID API Key" field.
+3. Set interval to your desire but make sure you stay under 500 requests per month (or about 16 per day)
+4. We even support other stock symbols but why would you care about those!
+4. Click "Save"
+
+Enjoy!
--- background.js
+++ background.js
@@ -1,14 +1,20 @@
 // fetch AMZN stock price and display as badge
 
-const YAHOO_API_KEY = ''; // put your own key here
-const SYMBOL = 'AMZN'
-const INTERVAL = 30
+let settings = {}; // persistent Chrome-sync storage for settings
 
-chrome.runtime.onInstalled.addListener(() => {
+chrome.runtime.onInstalled.addListener(async () => {
   updateBadge('init...');
+  // load settings
+  await initStorageCache;
+  settings.interval=parseInt(settings['interval']); // need int for this field
+  console.log ("settings loaded: "+JSON.stringify(settings));
+
+  // initial price
   updateBadgeWithStockprice();
-  chrome.alarms.create({ periodInMinutes: INTERVAL, delayInMinutes: INTERVAL});
-  console.log('Init complete');
+  
+  // set recurring event to update
+  chrome.alarms.create({ periodInMinutes: settings['interval'], delayInMinutes: settings['interval'] });
+  console.log('Init complete, will refresh '+settings['symbol']+' quote every '+settings['interval']+' minutes.');
 });
 
 chrome.action.onClicked.addListener(() => { 
@@ -23,13 +29,27 @@ chrome.alarms.onAlarm.addListener(() => {
   updateBadgeWithStockprice();
 });
 
+// Reads all data out of storage.sync and exposes it via a promise.
+const initStorageCache = loadOptions().then(items => {
+  Object.assign(settings, items);
+});
+function loadOptions() {
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.get(null, (items) => {
+      if (chrome.runtime.lastError) { return reject(chrome.runtime.lastError); }
+      resolve(items);
+    });
+  });
+}
+
+// Make call to Yahoo Finance API via Rapid API
 function updateBadgeWithStockprice() {
-  const url = 'https://yh-finance.p.rapidapi.com/market/v2/get-quotes?region=US&symbols='+SYMBOL;
+  const url = 'https://yh-finance.p.rapidapi.com/market/v2/get-quotes?region=US&symbols='+settings['symbol'];
   const options = {
     method: 'GET',
     headers: {
       'X-RapidAPI-Host': 'yh-finance.p.rapidapi.com',
-      'X-RapidAPI-Key': YAHOO_API_KEY
+      'X-RapidAPI-Key': settings['apikey']
     }
   };
   
@@ -41,7 +61,7 @@ function updateBadgeWithStockprice() {
       let prevClose = stock.regularMarketPreviousClose;
       let change = stock.regularMarketChange;
       let changePercent = stock.regularMarketChangePercent;
-      let tooltip = SYMBOL+" $"+price.toFixed(2)+" ("+change.toFixed(2)+", "+changePercent.toFixed(2)+"%)";
+      let tooltip = settings['symbol']+" $"+price.toFixed(2)+" ("+change.toFixed(2)+", "+changePercent.toFixed(2)+"%)";
       //console.log(price);
       if (price >= prevClose) {
         updateBadge(price.toFixed(0), tooltip, "blue")
@@ -49,12 +69,15 @@ function updateBadgeWithStockprice() {
         updateBadge(price.toFixed(0), tooltip, "red")
       }
     })
-    .catch(err => console.error('error:' + err));
+    .catch(err => {
+      updateBadge("failed");
+      console.error('error:' + err)
+    });
 };
 
-function updateBadge(text="", tooltip=SYMBOL, color="gray") {
+function updateBadge(text="", tooltip="", color="gray") {
   chrome.action.setBadgeText({text: text});
   chrome.action.setBadgeBackgroundColor({color: color});
   chrome.action.setTitle({title: tooltip})
   console.log('Badge updated to '+text+' at '+new Date);    
-};
+}
