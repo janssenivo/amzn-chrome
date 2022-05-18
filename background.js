2a3e1c8e28cbf9ea3a2e62214cd4a5eb17b9fc0b
Ivo Janssen <jansseni@amazon.com>
2022-05-18 13:04:41 -0500

    tooltip text, abstract vars

--- background.js
+++ background.js
@@ -1,28 +1,30 @@
 // fetch AMZN stock price and display as badge
 
 const YAHOO_API_KEY = ''; // put your own key here
+const SYMBOL = 'AMZN'
+const INTERVAL = 30
 
 chrome.runtime.onInstalled.addListener(() => {
   updateBadge('init...');
-  chrome.alarms.create({ periodInMinutes: 30, delayInMinutes: 30});
   updateBadgeWithStockprice();
+  chrome.alarms.create({ periodInMinutes: INTERVAL, delayInMinutes: INTERVAL});
   console.log('Init complete');
 });
 
 chrome.action.onClicked.addListener(() => { 
-  console.log('Extension clicked');
-  updateBadge('updating...');
+  console.log('Icon clicked');
+  updateBadge('');
   updateBadgeWithStockprice();
 });
 
 chrome.alarms.onAlarm.addListener(() => {
   console.log('Alarm triggered')
-  updateBadge('updating...');
+  updateBadge('');
   updateBadgeWithStockprice();
 });
 
 function updateBadgeWithStockprice() {
-  const url = 'https://yh-finance.p.rapidapi.com/market/v2/get-quotes?region=US&symbols=AMZN';
+  const url = 'https://yh-finance.p.rapidapi.com/market/v2/get-quotes?region=US&symbols='+SYMBOL;
   const options = {
     method: 'GET',
     headers: {
@@ -34,20 +36,25 @@ function updateBadgeWithStockprice() {
   fetch(url, options)
     .then(res => res.json())
     .then(json => {
-      let price = json.quoteResponse.result[0].regularMarketPrice.toFixed();
-      let prevClose = json.quoteResponse.result[0].regularMarketPreviousClose.toFixed();
+      let stock = json.quoteResponse.result[0];
+      let price = stock.regularMarketPrice;
+      let prevClose = stock.regularMarketPreviousClose;
+      let change = stock.regularMarketChange;
+      let changePercent = stock.regularMarketChangePercent;
+      let tooltip = SYMBOL+" $"+price.toFixed(2)+" ("+change.toFixed(2)+", "+changePercent.toFixed(2)+"%)";
       //console.log(price);
       if (price >= prevClose) {
-        updateBadge(price, "blue")
+        updateBadge(price.toFixed(0), tooltip, "blue")
       } else {
-        updateBadge(price,"red")
+        updateBadge(price.toFixed(0), tooltip, "red")
       }
     })
     .catch(err => console.error('error:' + err));
 };
 
-function updateBadge(text, color="gray") {
-  chrome.action.setBadgeBackgroundColor({color: color});
+function updateBadge(text="", tooltip=SYMBOL, color="gray") {
   chrome.action.setBadgeText({text: text});
+  chrome.action.setBadgeBackgroundColor({color: color});
+  chrome.action.setTitle({title: tooltip})
   console.log('Badge updated to '+text+' at '+new Date);    
 };
